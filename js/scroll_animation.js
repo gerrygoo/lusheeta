02ce440e415d57c3ce6b576a5018428c7f4ff1b2
Gerardo <gerrygoable@outlook.com>
2020-04-27 15:41:52 -0500

    update on windows resize

--- js/scroll_animation.js
+++ js/scroll_animation.js
@@ -1,16 +1,20 @@
 $(() => {
-  const controller = new ScrollMagic.Controller({
-    globalSceneOptions: {
-      triggerHook: 'onLeave',
-      duration: '200%',
-    },
-  })
+  const setScrollEffect = () => {
+    const controller = new ScrollMagic.Controller({
+      globalSceneOptions: {
+        triggerHook: 'onLeave',
+        duration: '200%',
+      },
+    })
 
-  document
-    .querySelectorAll('section.panel')
-    .forEach((element) =>
-      new ScrollMagic.Scene({ triggerElement: element })
-        .setPin(element, { pushFollowers: false })
-        .addTo(controller)
-    )
+    document
+      .querySelectorAll('section.panel')
+      .forEach(element =>
+        new ScrollMagic.Scene({ triggerElement: element })
+          .setPin(element, { pushFollowers: false })
+          .addTo(controller)
+      )
+  }
+  setScrollEffect()
+  window.onresize = setScrollEffect
 })
